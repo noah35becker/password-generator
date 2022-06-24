453925f8d72895d5975676d0acc9331283b4b9dd
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-06-24 16:55:29 -0400

    generatePassword() working w test (non-user-input) criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,93 @@
-// Assignment code here
+
+//////My code lies between the two long lines of slashes//////- Noah Becker////////////////////
+
+
+const CHARACTERS = {
+  lowercase: 'abcdefghijklmnopqrstuvwxyz',
+  uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numeric: '0123456789',
+  special: ' !"#$%&()*+,-.:;<=>?@[]^_`{|}~' + "'" + '\\'
+}
+
 
 function generatePassword(){
-  return "test";
+
+  //DECLARE VARIABLES
+    var password;
+    var eligibleChars;
+    
+    var passwordLength = 10; //test value
+    var useLowercase = true; //test value
+    var useUppercase = true; //test value
+    var useNumeric = true; //test value
+    var useSpecial = true; //test value
+
+  
+  //DECLARE INNER FUNCTIONS
+    function getEligibleChars(){
+      var output = '';
+
+      if (useLowercase)
+        output += CHARACTERS.lowercase;
+      if (useUppercase)
+        output += CHARACTERS.uppercase;
+      if (useNumeric)
+        output += CHARACTERS.numeric;
+      if (useSpecial)
+        output += CHARACTERS.special;
+        
+      return output;
+    }
+
+
+    function build(){
+      var output = '';
+
+      for (i = 0; i < passwordLength; i++){
+        var randIndex = Math.floor(Math.random() * eligibleChars.length);
+        output += eligibleChars.substring(randIndex, randIndex + 1);
+      }
+
+      return output;
+    }
+
+
+    function allCharCategoriesPresent(){
+      
+      function checkAgainstBaseline(baseline){
+        for (i = 0; i < password.length; i++)
+          for (c = 0; c < baseline.length; c++)
+            if (password.substring(i, i + 1) === baseline.substring(c, c + 1))
+              return true;
+              
+        return false;
+      }
+
+      if(useLowercase && !checkAgainstBaseline(CHARACTERS.lowercase))
+        return false;
+      if(useUppercase && !checkAgainstBaseline(CHARACTERS.uppercase))
+        return false;
+      if(useNumeric && !checkAgainstBaseline(CHARACTERS.numeric))
+        return false;
+      if(useSpecial && !checkAgainstBaseline(CHARACTERS.special))
+        return false;
+      
+      return true;
+
+    }
+
+
+  //EXECUTE
+    eligibleChars = getEligibleChars();
+    
+    do
+      password = build();
+    while (!allCharCategoriesPresent());
+
+    return password;
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 
 
