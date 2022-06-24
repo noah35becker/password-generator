5a6c1cf52d3a9d3285c27e6257c7eaa5e85f52e4
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-06-24 13:36:30 -0400

    generatePassword function created

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,23 @@
 // Assignment code here
 
+function generatePassword(){
+  return "test";
+}
+
+
+
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener('click', writePassword);
