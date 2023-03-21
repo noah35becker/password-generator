3f3c2dd9108a19e1b275c767c1b0e330edfc6b4c
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2023-03-21 17:32:23 -0400

    refactor copy btn js

--- assets/js/copy-to-clipboard.js
+++ assets/js/copy-to-clipboard.js
@@ -2,28 +2,34 @@
 // IMPORT
 import {passwordText} from './dom-elements.js'; 
 
-
 const copyBtn = document.querySelector('#copy-to-clipboard');
-const origCopyBtnText = copyBtn.innerHTML;
+const origText = 'Copy to clipboard';
+
 
 
 export function copyBtnReset(){
-    copyBtn.innerHTML = origCopyBtnText;
-    copyBtn.removeAttribute('style');
+    copyBtn.innerHTML = origText;
+    copyBtn.removeAttribute('style');  // as applied in the event listener below
 }
 
 
 copyBtn.addEventListener('click', function(){
-    const delay = 3000; //milliseconds
-    const duration = 1000; //milliseconds
+    const delay = 3000; // ms
+    const duration = 1000; // ms
 
     navigator.clipboard.writeText(passwordText.value);
     
-    copyBtn.style.width = window.getComputedStyle(copyBtn).getPropertyValue('width');
+    // Change text
+    copyBtn.style.width = window.getComputedStyle(copyBtn).getPropertyValue('width');  // prevent width of button from changing
     copyBtn.style.fontStyle = 'italic';
     copyBtn.innerHTML = 'Copied!';
-    copyBtn.style.transition = 'color ' + duration + 'ms ease-in ' + delay + 'ms';
-    copyBtn.style.color = window.getComputedStyle(copyBtn).getPropertyValue('background-color');
 
+    // Fade back to original text
+    copyBtn.style.transition = `color ${duration}ms ease-in ${delay}ms`;
+    copyBtn.style.color = window.getComputedStyle(copyBtn).getPropertyValue('background-color');
     setTimeout(copyBtnReset, delay + duration);
 });
+
+
+// On page load, set copy button text
+copyBtn.innerHTML = origText;
